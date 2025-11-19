669111c099b7b996b44d0d8a1ba377b5dc2db238
Djallex88 <willylem2010@gmail.com>
2025-11-18 21:35:28 -0500

    Changes: Mainly tags n shi

--- src/main/java/net/djallex/mccourse/block/ModBlocks.java
+++ src/main/java/net/djallex/mccourse/block/ModBlocks.java
@@ -6,7 +6,10 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,13 +22,16 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MCCourse.MOD_ID);
 
 
+        // Iron alexandrite Blocks
+        public static final RegistryObject<Block> ALEXANDRITE_BLOCK = registerBlock("alexandriteblock", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.METAL).instrument(NoteBlockInstrument.IRON_XYLOPHONE).requiresCorrectToolForDrops().strength(0.5f, 6.0f).sound(SoundType.METAL)) );
+        public static final RegistryObject<Block> RAW_ALEXANDRITE_BLOCK = registerBlock("rawalexandriteblock", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.METAL).instrument(NoteBlockInstrument.IRON_XYLOPHONE).requiresCorrectToolForDrops().strength(0.5f, 6.0f).sound(SoundType.METAL)) );
 
-        public static final RegistryObject<Block> ALEXANDRITE_BLOCK = registerBlock("alexandriteblock", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)) );
-        public static final RegistryObject<Block> RAW_ALEXANDRITE_BLOCK = registerBlock("rawalexandriteblock", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)) );
-        public static final RegistryObject<Block> ORE_ALEXANDRITE_BLOCK = registerBlock("alexandriteore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)) );
-        public static final RegistryObject<Block> DEEPORE_ALEXANDRITE_BLOCK = registerBlock("deepslatealexandriteore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)) );
-        public static final RegistryObject<Block> NETHERORE_ALEXANDRITE_BLOCK = registerBlock("netheralexandriteore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)) );
-    public static final RegistryObject<Block> ENDORE_ALEXANDRITE_BLOCK = registerBlock("endstonealexandriteore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)) );
+
+        // Ores
+        public static final RegistryObject<Block> ORE_ALEXANDRITE_BLOCK = registerBlock("alexandriteore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).strength(5f).requiresCorrectToolForDrops()) );
+        public static final RegistryObject<Block> DEEPORE_ALEXANDRITE_BLOCK = registerBlock("deepslatealexandriteore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).strength(5f).requiresCorrectToolForDrops()) );
+        public static final RegistryObject<Block> NETHERORE_ALEXANDRITE_BLOCK = registerBlock("netheralexandriteore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.NETHERRACK).strength(5f).requiresCorrectToolForDrops()) );
+        public static final RegistryObject<Block> ENDORE_ALEXANDRITE_BLOCK = registerBlock("endstonealexandriteore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.END_STONE).strength(5f).requiresCorrectToolForDrops()) );
 
 
 
