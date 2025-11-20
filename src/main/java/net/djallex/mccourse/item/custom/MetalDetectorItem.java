d83a1c39d1bc5cea346f94973e4df3efd105d3f5
Djallex88 <willylem2010@gmail.com>
2025-11-20 18:18:56 -0500

    Changes: added first custom item

--- src/main/java/net/djallex/mccourse/item/custom/MetalDetectorItem.java
+++ src/main/java/net/djallex/mccourse/item/custom/MetalDetectorItem.java
@@ -1,8 +1,15 @@
 package net.djallex.mccourse.item.custom;
 
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties pProperties) {
@@ -10,8 +17,46 @@ public class MetalDetectorItem extends Item {
     }
 
     @Override
-    public InteractionResult useOn(UseOnContext pContext) {
+    public InteractionResult useOn(UseOnContext pContext) /*called when clicked on a block*/{
+        if(!pContext.getLevel().isClientSide){
+
+            BlockPos positionClicked = pContext.getClickedPos();
+            Player player = pContext.getPlayer();
+            boolean foundBlock = false;
+
+            for(int i = 0 ; i <= positionClicked.getY() + 64; i++){
+
+                BlockState blockState = pContext.getLevel().getBlockState(positionClicked.below(i));
+
+                if(isValuableBlock(blockState)){
+
+                    outputValuableCoordinates(positionClicked.below(i), player, blockState.getBlock());
+                }
+
+
+            }
+
+
+        }
+
+
+
+
         return super.useOn(pContext);
+    }
+
+    private void outputValuableCoordinates(BlockPos below, Player player, Block block) {
+
+            player.sendSystemMessage(Component.literal("Valuable Found " + I18n.get(block.getDescriptionId()) + "at (" + below.getX() + ", " + below.getY() + ", " + below.getZ() + ") "));
+
+
+
+    }
+
+    private boolean isValuableBlock(BlockState blockState) {
+
+        return blockState.is(Blocks.IRON_ORE)  || blockState.is(Blocks.DEEPSLATE_IRON_ORE)  || blockState.is(Blocks.DIAMOND_ORE)  || blockState.is(Blocks.DEEPSLATE_DIAMOND_ORE);
+
     }
 }
 
